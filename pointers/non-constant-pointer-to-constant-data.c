e9da95c1ae2e57161ca488fda55e2a2660302360
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-03-26 10:27:16 +0000

    print data of a constant data by non constant pointer

---
+++ pointers/non-constant-pointer-to-constant-data.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void printCharacters(const char *sPtr);
+
+int main(void)
+{
+    char string[] = "print characters of string";
+
+    puts("The string is: ");
+    printCharacters(string);
+    puts("");
+}
+
+void printCharacters(const char *sPtr)
+{
+    for (; *sPtr != '\0'; ++sPtr)
+    {
+        printf("%c", *sPtr);
+    }
+}
