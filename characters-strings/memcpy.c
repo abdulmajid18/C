4e54532b38b88075f7eb1e38d7d6100449a7233c
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-04-01 14:09:28 +0000

    copy actual object from memory

---
+++ characters-strings/memcpy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char s1[17];
+    char s2[] = "Copy this string";
+
+    memcpy(s1, s2, 17);
+    memcpy(s1, s2, 17);
+    printf("%s\n%s\"%s\"\n",
+           "After s2 is copied into s1 with memcpy,",
+           "s1 contains ", s1);
+}
