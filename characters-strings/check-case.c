28f9d53a1f0001c151bb80c338800e1e93d84c7d
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-04-01 12:40:38 +0000

    use isdigit

--- characters-strings/check-case.c
+++ characters-strings/check-case.c
@@ -8,4 +8,20 @@ int main(void)
            isdigit('8') ? "8 is a" : " 8 is not a ",
            "digit",
            isdigit('#') ? "# is a" : "# is not a", " digit");
+
+    printf("%s\n%s%s\n%s%s\n%s%s\n\n",
+           "According to isalnum:",
+           isalnum('A')
+               ? "A is a "
+               : "A is not a ",
+           "digit or a letter",
+           isalnum('8')
+               ? "8 is a "
+               : "8 is not a ",
+           "digit or a letter",
+           isalnum('#')
+               ? "# is a "
+               : "# is not a ",
+           "digit or a letter");
 }
+s
